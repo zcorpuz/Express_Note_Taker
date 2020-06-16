7dd660b4f17be50c40bc7efff1c91fcd2059d0ca
Zach Corpuz <zachcorpuz@yahoo.com>
2020-06-15 23:35:56 -0700

    reorganized structure of files. Finished first go at the API calls

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 // Dependencies
 const express = require("express");
-// Do we need this? const fs = require("fs");
+const fs = require("fs");
+const db = require("../Develop/db/db.json");
 
 // Tells node that we are creating an "express" server
 const app = express();
@@ -17,26 +18,31 @@ app.use(express.json());
 
 // API Routes
 // =====================================================
-// Need to set up GET API for /api/notes- Should read the db.json file and return all saves notes as JSON
 app.get('/api/notes', (req, res) => 
-    // fs.readFile("../assets/public/db/db.json", 'utf8', (err, data) => {
-    //     if(err) {
-    //         console.log(err);
-    //     } else {
-    //         return data;
-    //     }
-    // });
-    return res.json(//need to create a variable for db.json);
+    fs.readFile(db, 'utf8', (err, data) => {
+        if(err) {
+            console.log(err);
+        } else {
+            return data;
+        }
+    })
 );
 // Neet to set up POST API for /api/notes- Should receive a new note to save on the request body, addit to the db.json file, then return the new note to the client
 app.post('/api/notes', (req, res) => {
+    const newNotes = req.body;
 
+    newNotes.routeName = newNotes.replace(/\s+/g, "").toLowerCase();
+
+    fs.appendFile(db, newNotes, (err, data) => {
+        if (err) throw err;
+        res.json.(newNotes);
+    });
 })
 
 // HTML Routes
 // =====================================================
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, "../assets/notes.html")));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, "../assets/index.html")));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, "public/index.html")));
 
 // Server Listener
 // =====================================================
