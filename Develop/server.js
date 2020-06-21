e9f1756ee3a6fde76857890e24328f54ae43e7b5
Zach Corpuz <zachcorpuz@yahoo.com>
2020-06-20 21:36:25 -0700

    added path for / to homepage

--- Develop/server.js
+++ Develop/server.js
@@ -70,6 +70,7 @@ app.delete('/api/notes/:id', (req, res) => {
 
 // HTML Routes
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, "./public/notes.html")));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "public/index.html")));
 
 // Server Listener
 app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`));
